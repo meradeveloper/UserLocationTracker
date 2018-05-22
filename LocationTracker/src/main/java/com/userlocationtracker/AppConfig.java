c5576edf101ed0874526e6b109100612e24f9549
meradeveloper <rawatsumit115@gmail.com>
2018-05-22 13:02:22 +0530

    22/5/18 stop reciever added

--- LocationTracker/src/main/java/com/userlocationtracker/AppConfig.java
+++ LocationTracker/src/main/java/com/userlocationtracker/AppConfig.java
@@ -3,7 +3,6 @@ package com.userlocationtracker;
 import android.app.Application;
 import android.content.SharedPreferences;
 
-import org.greenrobot.eventbus.EventBus;
 
 /**
  * Created by Localadmin on 5/22/2018.
@@ -11,20 +10,13 @@ import org.greenrobot.eventbus.EventBus;
 
 public class AppConfig extends Application {
 
-    private static EventBus mEventBus;
     private static SharedPreferences mPrefs;
     private String LOCATIONPREFERENCE="LocationPreference";
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        mEventBus=EventBus.getDefault();
         mPrefs = getSharedPreferences(LOCATIONPREFERENCE,MODE_PRIVATE);
-    }
-
-    public static EventBus getEventBus() {
-        return mEventBus;
     }
 
     public static SharedPreferences getPrefs() {
